0efa61d73707dbaff3a4ffa51943c7aa85d40395
TrungVV <trungvv@lungvang.vn>
2018-11-29 09:57:43 +0700

    update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
 import logo from '../img/logo.jpg'
 
 const Navbar = () => (
-  <nav className="navbar is-transparent">
+  <nav className="navbar is-transparent is-white is-fixed-top">
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
@@ -12,35 +11,62 @@ const Navbar = () => (
             <img src={logo} alt="Hai Nam Media" style={{ width: '88px' }} />
           </figure>
         </Link>
+        <div className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navMenu">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </div>
       </div>
-      <div className="navbar-start">
+      <div id="navMenu" className="navbar-end">
         <Link className="navbar-item" to="/about">
-          About
+          Giới thiệu
         </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
+        <div className="navbar-item has-dropdown is-hoverable"> 
+          <Link className="navbar-link" to="/products">
+            Sản Phẩm
+          </Link>
+          <div className="navbar-dropdown is-boxed">
+            <Link className="navbar-item" to="">Phim</Link>
+            <Link className="navbar-item" to="">Chương trình tin tức</Link>
+            <Link className="navbar-item" to="">Game Shows</Link>
+            <Link className="navbar-item" to="">Chương trình xã hội</Link>
+          </div>
+        </div>
+        <div className="navbar-item has-dropdown is-hoverable"> 
+          <Link className="navbar-link" to="/services">
+            Dịch vụ
+          </Link>
+          <div className="navbar-dropdown is-boxed">
+            <Link className="navbar-item" to="">Media</Link>
+            <Link className="navbar-item" to="">Public Relations</Link>
+            <Link className="navbar-item" to="">Tổ chức sự kiện</Link>
+            <Link className="navbar-item" to="">Digital marketing</Link>
+            <Link className="navbar-item" to="">OOH</Link>
+            <Link className="navbar-item" to="">Dịch vụ khác</Link>
+          </div>
+        </div>
         <Link className="navbar-item" to="/contact">
-          Contact
+          Khách hàng
         </Link>
         <Link className="navbar-item" to="/contact/examples">
-          Form Examples
+          Bảng giá
         </Link>
+        <div className="navbar-item has-dropdown is-hoverable">
+          <Link className="navbar-link" to="">
+            VN
+          </Link>
+          <div className="navbar-dropdown is-boxed">
+            <Link className="navbar-item" to="">
+              EN
+            </Link>
+          </div>
+        </div>
       </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
-      </div>
+      
     </div>
+    
   </nav>
+  
 )
 
 export default Navbar
